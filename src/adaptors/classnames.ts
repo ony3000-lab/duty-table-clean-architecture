1c79e009221783ca999d42fb63c0e2f25f2b7bbe
Hyeonjong <nianelo4@gmail.com>
2023-12-22 11:15:02 +0900

    Remove unused things

--- src/adaptors/classnames.ts
+++
@@ -1,6 +0,0 @@
-import type { ArgumentArray } from 'classnames';
-import classNames from 'classnames';
-
-export default function enhancedClassNames(...args: ArgumentArray): string {
-  return classNames(args).replace(/\s+/g, ' ');
-}
