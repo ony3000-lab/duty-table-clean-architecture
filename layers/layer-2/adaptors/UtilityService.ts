463fd691eca1d0588bda4a110aabc29e795029b3
Hyeonjong <nianelo4@gmail.com>
2024-01-02 11:30:12 +0900

    Update UtilityService

--- layers/layer-2/adaptors/UtilityService.ts
+++ layers/layer-2/adaptors/UtilityService.ts
@@ -1,16 +1,8 @@
 import type { UtilityService } from 'layer-1/ports';
+import { shuffle } from 'radash';
 
 export const utilityService: UtilityService = {
   shuffle(list) {
-    if (list.length <= 1) {
-      return list;
-    }
-
-    const shuffledIndexes = [...Array(list.length)]
-      .map((_, index) => ({ index, key: Math.random() }))
-      .sort((former, latter) => former.key - latter.key)
-      .map(({ index }) => index);
-
-    return list.map((_, index) => list[shuffledIndexes[index]]);
+    return shuffle(list);
   },
 };
